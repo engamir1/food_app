26d1fa5c7005c7cb95dd4da9ac3603a813989582
engamir1 <engmohamed.amir101@gmail.com>
2024-01-14 02:25:21 +0200

    add signls to models

---
+++ accounts/signals.py
@@ -0,0 +1,17 @@
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+
+from accounts.models import User, UserProfile
+
+
+@receiver(post_save, sender=User)
+def create_profile(sender, instance, created, **kwargs):
+    if created:
+        UserProfile.objects.create(user=instance)
+    else:
+        try:
+            profile = UserProfile.objects.get(user=instance)
+            profile.save()
+        except UserProfile.DoesNotExist:
+            UserProfile.objects.create(user=instance)
+            print("Profile created")
