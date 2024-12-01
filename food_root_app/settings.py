8b2be2179ef24b6d6cf826f509a459ebee466c90
engamir1 <engmohamed.amir101@gmail.com>
2024-12-01 22:39:49 +0200

    use ready templates

--- food_root_app/settings.py
+++ food_root_app/settings.py
@@ -117,6 +117,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+STATICFILES_DIRS = ['food_root_app/static']
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
